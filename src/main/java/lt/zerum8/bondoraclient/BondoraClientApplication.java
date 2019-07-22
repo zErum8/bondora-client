927ed907afd0ca6023849e5b0bbe374838d75438
zErum8 <zeniateo@gmail.com>
2019-07-22 22:58:17 +0300

    Add generated spring-boot sources

---
+++ src/main/java/lt/zerum8/bondoraclient/BondoraClientApplication.java
@@ -0,0 +1,13 @@
+package lt.zerum8.bondoraclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BondoraClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BondoraClientApplication.class, args);
+	}
+
+}
